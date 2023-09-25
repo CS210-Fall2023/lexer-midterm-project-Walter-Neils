153df2c7dd5e599fb8866de6385e9d1d12cdf543
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2023-09-25 16:40:18 -0700

    Fixed testing script

--- test.sh
+++ test.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
+
+# Usage: print_success <message>
+print_success() {
+    printf "\e[0;32m  [✔] %s\e[0m\n" "$1"
+}
+
+# Usage: print_error <message>
+print_error() {
+    printf "\e[0;31m  [✖] %s %s\e[0m\n" "$1" "$2"
+}
+
+# Usage: print_info <message>
+print_info() {
+    printf "\e[0;33m  [ℹ] %s\e[0m\n" "$1"
+}
+
 for file in files/*.cci; do
     ./lexer/lexer $file >${file}.lexer
-    echo "${file}.ANS vs. ${file}.lexer"
+    print_info "${file}.ANS vs. ${file}.lexer"
     #diff ${file}.ANS  ${file}.lexer
     if [[ $(diff ${file}.ANS ${file}.lexer) ]]; then #if output of diff command is not zero
-        echo "Somthing went wrong"
-        # diff ${file}.ANS ${file}.lexer
-        cat ${file}.ANS | xclip -selection clipboard
-        echo "Copied answer to clipboard"
-        read -p "Press enter to continue"
-        cat ${file}.lexer | xclip -selection clipboard
-        echo "Copied your answer to clipboard"
-        echo
-        read -p "Press enter to continue"
+        print_error "Test failed"
+        vimdiff ${file}.ANS ${file}.lexer
     else
-        echo "You passed the test"
-        echo
+        print_success "You passed the test"
     fi
-    # Wait for enter key
-
 done
 
 for file in files/*.ccx; do
     ./lexer/lexer $file >${file}.lexer
-    echo "${file}.ANS vs. ${file}.lexer"
+    print_info "${file}.ANS vs. ${file}.lexer"
     #diff ${file}.ANS  ${file}.lexer
     if [[ $(diff ${file}.ANS ${file}.lexer) ]]; then #if output of diff command is not zero
-        echo "Somthing went wrong"
-        # diff ${file}.ANS ${file}.lexer
-        cat ${file}.ANS | xclip -selection clipboard
-        echo "Copied answer to clipboard"
-        read -p "Press enter to continue"
-        cat ${file}.lexer | xclip -selection clipboard
-        echo "Copied your answer to clipboard"
-        read -p "Press enter to continue"
-        echo
+        print_error "Test failed"
+        vimdiff ${file}.ANS ${file}.lexer
     else
-        echo "You passed the test"
-        echo
+        print_success "You passed the test"
     fi
 done
